1dff45c1c8a4fedf7d50fc39c563173a289b9dd4
Onkar-001 <onkar.shinde.432@gmail.com>
2025-02-19 16:22:24 +0530

    task123 is commited

--- test1.java
+++ test1.java
@@ -6,12 +6,10 @@ public class test1 {
 		System.out.println("MSG from DEV2");
 		System.out.println("Dev2 doing changes");
 	}
-<<<<<<< Updated upstream
 	public void m2() {
 		System.out.println("test456 methods is going on...");
-=======
+	}	
 	public void m1() {
 		System.out.println("task123 is going on...");
->>>>>>> Stashed changes
 	}
 }
