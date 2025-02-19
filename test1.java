5082701078cb8d001d399496a34467d1b0961831
Onkar-001 <onkar.shinde.432@gmail.com>
2025-02-19 13:39:03 +0530

    dev1 add var a = 10

--- test1.java
+++ test1.java
@@ -1,5 +1,7 @@
 public class test1 {
 	public static void main(String[] args) {
+		int a = 20;
+		System.out.println(a);
 		System.out.println("Hello");
 		System.out.println("MSG from DEV2");
 		System.out.println("Dev2 doing changes");
